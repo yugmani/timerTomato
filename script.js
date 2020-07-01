44f574a7589c7ab88c5439b83f3c3640eff891bc
Himalaya <yugmani@yahoo.com>
2020-06-30 23:42:13 -0700

    update

--- script.js
+++ script.js
@@ -12,44 +12,100 @@ var totalSeconds = 0;
 var secondsElapsed = 0;
 var interval;
 var myVar;
+var isPaused = false;
+var seconds;
 
 function startTimer() {
+  isPaused = false;
+  clearInterval(myVar);
+  stopButton.classList.remove("active");
+  pauseButton.classList.remove("active");
+  playButton.classList.add("active");
+
   // Write code to start the timer here
-  var totalMinutes = workMinutesInput.value;
-  if (totalMinutes <= 0){
+  interval = workMinutesInput.value;
+  if (interval <= 0){
     alert("Not valid");
     return;
   }
-  var seconds = 60;
-  myVar = setInterval(myTimer, 1000);
-
-  function myTimer() {
-    // totalMinutes -= 1;
-    seconds -= 1;
-    
-      if ( (totalMinutes <= 0) && (seconds <= 0 ) ){
-        clearInterval(myVar);
-        pauseTimer();
-      }
-       else if (seconds <= 0) {
-          seconds = 60;
-          totalMinutes -= 1;
-        } 
-      
-      secondsDisplay.innerHTML = seconds;
-      minutesDisplay.innerHTML = totalMinutes;
-      }
+  else {
+    seconds = 60;
+    myVar = setInterval( myTimer, 1000);
+  }
  }
     
+
+ function myTimer() {
+    if (isPaused === false ){
+    // interval -= 1;
+    seconds -= 1;
+
+    if ( (interval <= 0) && (seconds <= 0 ) ){
+      
+      restTimer();
+      clearinterval(myVar);
+    }
+    else if (seconds <= 0) {
+        seconds = 60;
+        interval -= 1;
+      } 
+
+    secondsDisplay.innerHTML = seconds;
+    minutesDisplay.innerHTML = interval- 1;
+    }
+    else return;
+ }
+
+
 function pauseTimer() {
-    clearInterval(myVar);
+  if (!isPaused){
+    isPaused = true;
+  }
+  else {
+    isPaused = false;
+  }
+  playButton.classList.remove("active");
+  stopButton.classList.remove("active");
+  pauseButton.classList.toggle("active");
 }
 
 function stopTimer(){
-    window.stop();
+  clearInterval(myVar);
+  seconds = 0;
+  interval = 0;
+  stopButton.setAttribute("class", "active");
+  playButton.classList.remove("active");
+  pauseButton.classList.remove("active");
 }
 
-// startTimer();
+
+function restTimer() {
+  restInterval = 5;
+  restSeconds = 60;
+
+  var restVar = setInterval(restTimer, 1000);
+}
+
+function restTimer() {
+  if (isPaused === false ){
+  // interval -= 1;
+  restSeconds -= 1;
+
+  if ( (restInterval <= 0) && (restSeconds <= 0 ) ){
+    clearintervalerval(restVar);
+    pauseTimer();
+  }
+  else if (restSeconds <= 0) {
+      restSeconds = 60;
+      restInterval -= 1;
+    } 
+
+  secondsDisplay.innerHTML = restSeconds;
+  minutesDisplay.innerHTML = restInterval- 1;
+  }
+  else return;
+}
+
 playButton.addEventListener("click", startTimer);
 pauseButton.addEventListener("click", pauseTimer);
 stopButton.addEventListener("click", stopTimer);
